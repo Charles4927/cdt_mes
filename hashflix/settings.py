701fe77c7fc7d8ff3419570ac6c84f8cf5169e25
Charles <charles4927@gmail.com>
2023-10-03 08:54:48 -0300

    ob

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -20,15 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# TOKEN_CSRF = os.getenv('TOKEN_CSRF')
-# if TOKEN_CSRF:
-#     SECRET_KEY = TOKEN_CSRF
-#     CSRF_TRUSTED_ORIGINS = ['cdtmes-production.up.railway.app']
-# else:
-#     SECRET_KEY = 'django-insecure-hx@+hr^6fzb!p7$2*&!)d(ryfgcx)6g4i(x3qu&th91v2^)32c'
-#
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+TOKEN_CSRF = os.getenv('TOKEN_CSRF')
+if TOKEN_CSRF:
+    SECRET_KEY = TOKEN_CSRF
+    CSRF_TRUSTED_ORIGINS = ['https://cdtmes-production.up.railway.app']
+else:
+    SECRET_KEY = 'django-insecure-hx@+hr^6fzb!p7$2*&!)d(ryfgcx)6g4i(x3qu&th91v2^)32c'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -92,13 +92,13 @@ DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DI
 
 import dj_database_url
 
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
-    }
+# DATABASE_URL = os.getenv("DATABASE_URL")
+# if DATABASE_URL:
+#     DATABASES = {
+#         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+#     }
 
 
 # Password validation
